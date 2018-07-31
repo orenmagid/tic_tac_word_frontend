d6c750377e55dc791404e8ca1f198b099926e6c8
orenmagid <autoimpedicis@gmail.com>
2018-07-31 13:39:13 -0400

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-word_array = ["jump", "gun", "fight", "cooperate", "chair", "hair", "care", "dare", "fun", "computer", "steady", "abate", "aberrant", "abjure", "abscond", "abstain", "disinterested", "disparage", "diverge", "impudent", "inadvertent", "precipitate", "zeal", "whimsical", "vexation", "venerate", "tortuous", "timorous", "soporific", "sediment", "placate", "opportunism", "opaque"]
+word_array = ["jump", "gun", "fight", "cooperate", "chair", "hair", "care", "dare", "fun", "computer", "steady", "abate", "aberrant", "abjure", "abscond", "abstain", "disinterested", "disparage", "diverge",
+  "impudent", "inadvertent", "precipitate", "zeal", "whimsical", "vexation", "venerate", "tortuous", "timorous", "soporific", "sediment", "placate", "opportunism", "opaque", "ephemeral", "eminent", "empirical",
+  "eloquence", "elicit", "discern", "dirge", "didactic", "determinant", "covet", "craft", "copious", "contend", "chauvinist", "chicanary", "help", "go", "die", "fail", "pass", "excel"]
 
 
 word_array.each do |value|

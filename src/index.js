799354af75c243ff05a3f592c1c803c5c90534f2
orenmagid <autoimpedicis@gmail.com>
2018-07-30 21:49:04 -0400

    Finished clear board functionality.

--- src/index.js
+++ src/index.js
@@ -256,4 +256,14 @@ function declareWinner(winningSymbol) {
   }
 }
 
-function clearBoard() {}
+function clearBoard() {
+  document.getElementById("r1c1").innerHTML = "";
+  document.getElementById("r1c2").innerHTML = "";
+  document.getElementById("r1c3").innerHTML = "";
+  document.getElementById("r2c1").innerHTML = "";
+  document.getElementById("r2c2").innerHTML = "";
+  document.getElementById("r2c3").innerHTML = "";
+  document.getElementById("r3c1").innerHTML = "";
+  document.getElementById("r3c2").innerHTML = "";
+  document.getElementById("r3c3").innerHTML = "";
+}

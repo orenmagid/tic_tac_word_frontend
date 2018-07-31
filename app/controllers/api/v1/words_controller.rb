c23300071064609ab67d64463bd3d1948c56e7b0
orenmagid <autoimpedicis@gmail.com>
2018-07-31 18:58:36 -0400

    patchWord fixes

--- app/controllers/api/v1/words_controller.rb
+++ app/controllers/api/v1/words_controller.rb
@@ -1,8 +1,30 @@
 class Api::V1::WordsController < ApplicationController
 
+  before_action :find_word, only: [:update]
+
   def index
     @word = Word.all.sample
     render json: @word, include: ['users']
+  end
+
+  def update
+    @word.update(word_params)
+    if @word.save
+      render json: @word, status: :accepted
+    else
+      render json: { errors: @word.errors.full_messages }, status: :unprocessible_entity
+    end
+
+  end
+
+  private
+
+  def word_params
+    params.permit(:id, :label, :user_ids)
+  end
+
+  def find_word
+    @word = Word.find(params[:id])
   end
 
 

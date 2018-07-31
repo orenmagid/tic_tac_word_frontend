7bc41f275187f207cf3f18162f988f402d324278
orenmagid <autoimpedicis@gmail.com>
2018-07-31 08:22:27 -0400

    Some new seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-word_array = ["word", "smooth", "guess", "joke", "table", "goofy", "carpet", "breakfast", "lunch"]
+word_array = ["jump", "gun", "fight", "cooperate", "chair", "hair", "care", "dare", "fun", "computer", "steady", "abate", "aberrant", "abjure", "abscond", "abstain", "disinterested", "disparage", "diverge", "impudent", "inadvertent", "precipitate", "zeal", "whimsical", "vexation", "venerate", "tortuous", "timorous", "soporific", "sediment", "placate", "opportunism", "opaque"]
 
-word_array.each do |word|
-  Word.create(label: word)
+
+word_array.each do |value|
+  Word.create(label: value)
 end
